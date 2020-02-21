206e0958b45fcc0f548a9c63ff20200659b84611
Jerrain Yohann <kryod@users.noreply.github.com>
2020-02-21 13:46:04 +0100

    and god said 'let ther be sound'

---
+++ include/locomotive/sound.h
@@ -0,0 +1,32 @@
+#ifndef LOCOMOTIVE_SOUND_H
+#define LOCOMOTIVE_SOUND_H
+
+#include "locomotive/locomotive.h"
+#include "locomotive/threadpool.h"
+#include "irrKlang.h"
+
+#include "glm/vec3.hpp"
+
+namespace Locomotive {
+
+    class LOCOMOTIVE_API Sound {
+        public:
+            Sound();
+            Sound(int nb_thread);
+            ~Sound();
+
+            void playSound(const std::string& filename);
+            void playSound(const std::string& filename, bool repeat);
+            void playSound(const std::string& filename, glm::vec3 position);
+            void playSound(const std::string& filename, glm::vec3 position, bool repeat);
+            void updateListener(glm::vec3 position, glm::vec3 lookDirection);
+
+        private:
+            irrklang::ISoundEngine* engine;
+            Threadpool threadpool;
+            //std::vector<std::future<irrklang::ISound*> > sounds;
+    };
+
+}
+
+#endif
---
+++ src/sound.cpp
@@ -0,0 +1,48 @@
+#include "locomotive/sound.h"
+#include "glm/vec3.hpp"
+#include <iostream>
+
+namespace Locomotive {
+
+    Sound::Sound() {
+        this->engine = irrklang::createIrrKlangDevice();
+        threadpool.start(2);
+    }
+
+    Sound::Sound(int nb_thread) {
+        this->engine = irrklang::createIrrKlangDevice();
+        threadpool.start(nb_thread);
+    }
+
+    Sound::~Sound() {
+        threadpool.finish();
+
+        this->engine->stopAllSounds();
+        
+        this->engine->drop();
+    }
+
+    void Sound::playSound(const std::string& filename) {
+        this->threadpool.queue([&engine = engine, &filename]{engine->play2D(filename.c_str());});
+    }
+
+    void Sound::playSound(const std::string& filename, bool repeat) {
+        this->threadpool.queue([&engine = engine, &filename, repeat]{engine->play2D(filename.c_str(), repeat);});
+    }
+
+    void Sound::playSound(const std::string& filename, glm::vec3 position) {
+        irrklang::vec3df pos(position.x, position.y, position.z);
+        this->threadpool.queue([&engine = engine, &filename, &pos]{engine->play3D(filename.c_str(), pos);});
+    }
+
+    void Sound::playSound(const std::string& filename, glm::vec3 position, bool repeat) {
+        irrklang::vec3df pos(position.x, position.y, position.z);
+        this->threadpool.queue([&engine = engine, &filename, &pos, repeat]{engine->play3D(filename.c_str(), pos, repeat);});
+    }
+
+    void Sound::updateListener(glm::vec3 position, glm::vec3 lookDirection) {
+        this->engine->setListenerPosition(irrklang::vec3df(position.x, position.y, position.z),
+                                  irrklang::vec3df(lookDirection.x, lookDirection.y, lookDirection.z));
+ 
+    }
+}
