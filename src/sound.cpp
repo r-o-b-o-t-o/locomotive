114af39b4df4eacb85a2592fd17266210d5e92af
Axel Cocat <ax.cocat@gmail.com>
2020-02-21 14:40:47 +0100

    Fixed sound on windows, removed obj loader debug text

--- src/sound.cpp
+++ src/sound.cpp
@@ -23,21 +23,21 @@ namespace Locomotive {
     }
 
     void Sound::playSound(const std::string& filename) {
-        this->threadpool.queue([&engine = engine, &filename]{engine->play2D(filename.c_str());});
+        this->threadpool.queue([&engine = engine, filename] { engine->play2D(filename.c_str()); });
     }
 
     void Sound::playSound(const std::string& filename, bool repeat) {
-        this->threadpool.queue([&engine = engine, &filename, repeat]{engine->play2D(filename.c_str(), repeat);});
+        this->threadpool.queue([&engine = engine, filename, repeat]{engine->play2D(filename.c_str(), repeat);});
     }
 
     void Sound::playSound(const std::string& filename, glm::vec3 position) {
         irrklang::vec3df pos(position.x, position.y, position.z);
-        this->threadpool.queue([&engine = engine, &filename, &pos]{engine->play3D(filename.c_str(), pos);});
+        this->threadpool.queue([&engine = engine, filename, &pos]{engine->play3D(filename.c_str(), pos);});
     }
 
     void Sound::playSound(const std::string& filename, glm::vec3 position, bool repeat) {
         irrklang::vec3df pos(position.x, position.y, position.z);
-        this->threadpool.queue([&engine = engine, &filename, &pos, repeat]{engine->play3D(filename.c_str(), pos, repeat);});
+        this->threadpool.queue([&engine = engine, filename, &pos, repeat]{engine->play3D(filename.c_str(), pos, repeat);});
     }
 
     void Sound::updateListener(glm::vec3 position, glm::vec3 lookDirection) {
